ba8cb05998f6bbc7b8d5729c02d79f4793bcf38a
brunobilandzic <bruno.bilandzic@outlook.com>
2025-11-05 10:53:14 +0100

    correct api call

--- app/utils/auth.js
+++ app/utils/auth.js
@@ -2,7 +2,7 @@ import { useSession } from "next-auth/react";
 
 export const sessionAppUserClient = async () => {
   const session = useSession();
-  const res = await fetch("api/auth/user");
+  const res = await fetch("/api/auth/user");
   if (res.ok) {
     const appUser = await res.json();
     return {
