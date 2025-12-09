8f68c6b67e203a782c0cdc24625555522c9f78c0
brunobilandzic <bruno.bilandzic@outlook.com>
2025-12-09 11:16:54 +0100

    refactor: integrate AI generation option into new parameters page

--- components/blog/parameters/new.js
+++ components/blog/parameters/new.js
@@ -2,41 +2,60 @@
 
 // page component for new blog parameters page
 
-import BlogParametersForm from "@/components/UI/forms/parameters";
-import { MdArrowDownward, MdOpenInNew } from "react-icons/md";
+import BlogParametersForm, {
+  AIGenerateParametersForm,
+} from "@/components/UI/forms/parameters";
+import { MdArrowDownward, MdArrowForward, MdOpenInNew } from "react-icons/md";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-const NewParameters = () => (
-  <div className="flex flex-col justify-center gap-2 pt-4   ">
-    <NewParametersHeader />
-    <BlogParametersForm />
-  </div>
-);
+const NewParameters = () => {
+  const [generate, setGenerate] = useState(false);
+  return (
+    <div className="flex flex-col justify-center gap-2 pt-4   ">
+      <NewParametersHeader generate={generate} setGenerate={setGenerate} />
+      {generate ? <AIGenerateParametersForm /> : <BlogParametersForm />}
+    </div>
+  );
+};
 
-function NewParametersHeader() {
+export function NewParametersHeader({ generate, setGenerate }) {
   const router = useRouter();
 
   return (
     <div className="flex justify-between items-end mb-2">
-      <div className="font-bold flex items-center gap-2 text-xl">
+      <div
+        onClick={() => setGenerate(!generate)}
+        className="btn  flex items-center justify-end gap-2"
+      >
         Manual{" "}
-        <div className="mt-1">
-          <MdArrowDownward />
-        </div>
+        {generate ? (
+          <div className="mt-1">
+            <MdArrowForward />
+          </div>
+        ) : (
+          <div className="mt-1">
+            <MdArrowDownward />
+          </div>
+        )}
       </div>
       <div
-        onClick={() => router.push("/blog/parameters/new/generate")}
+        onClick={() => setGenerate(!generate)}
         className="btn  flex items-center justify-end gap-2"
       >
         <div> AI generate</div>
-        <div>
-          {" "}
-          <MdOpenInNew />
-        </div>
+        {generate ? (
+          <div className="mt-1">
+            <MdArrowDownward />
+          </div>
+        ) : (
+          <div className="mt-1">
+            <MdArrowForward />
+          </div>
+        )}
       </div>
     </div>
   );
 }
-
 
 export default NewParameters;
